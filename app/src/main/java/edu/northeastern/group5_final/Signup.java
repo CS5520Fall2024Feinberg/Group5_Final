06329b5696f1616657b18478ed454289b281244f
Nile <nnaim36@gmail.com>
2024-11-18 03:57:11 -0500

    added signup page.

--- app/src/main/java/edu/northeastern/group5_final/Signup.java
+++ app/src/main/java/edu/northeastern/group5_final/Signup.java
@@ -1,8 +1,6 @@
 package edu.northeastern.group5_final;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,20 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class Signup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-        TextView txtSignup = findViewById(R.id.txt_signup);
-        txtSignup.setOnClickListener(view -> {
-            Intent intent = new Intent(MainActivity.this, Signup.class);
-            startActivity(intent);
-        });
-
+        setContentView(R.layout.activity_signup);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
