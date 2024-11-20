98e90dcb4db2f47b6838726ca5a4b40c43e40dc9
Nile <nnaim36@gmail.com>
2024-11-20 05:30:38 -0500

    added logic for signing in that checks username and password.

--- app/src/main/java/edu/northeastern/group5_final/MainActivity.java
+++ app/src/main/java/edu/northeastern/group5_final/MainActivity.java
@@ -2,13 +2,23 @@ package edu.northeastern.group5_final;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +32,58 @@ public class MainActivity extends AppCompatActivity {
         txtSignup.setOnClickListener(view -> {
             Intent intent = new Intent(MainActivity.this, Signup.class);
             startActivity(intent);
+        });
+
+        EditText etxtUsername = findViewById(R.id.etxt_username);
+        EditText etxtPassword = findViewById(R.id.etxt_password);
+
+
+        Button btnLogin = findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(view -> {
+
+            String username = etxtUsername.getText().toString().trim();
+            String password = etxtPassword.getText().toString().trim();
+
+            //check if username and password are empty
+            if (username.isEmpty() || password.isEmpty()) {
+                Toast.makeText(MainActivity.this, "Please enter both username and password", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference usersRef = database.getReference("users");
+
+            usersRef.orderByChild("username").equalTo(username).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if(snapshot.exists()){
+                        for (DataSnapshot userSnapshot : snapshot.getChildren()){
+                            String storedPassword = userSnapshot.child("password").getValue(String.class);
+                            if(storedPassword.equals(password) && storedPassword != null){
+                                Toast.makeText(MainActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(MainActivity.this, loginPage.class);
+                                intent.putExtra("username", username);
+                                startActivity(intent);
+                                finish();
+                                return;
+                            }
+                            else{
+                                Toast.makeText(MainActivity.this, "Incorrect password", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                    else{
+                        Toast.makeText(MainActivity.this, "Username not found", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Toast.makeText(MainActivity.this, "Login failed. Please try again." , Toast.LENGTH_SHORT).show();
+                }
+            });
+
+
         });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
