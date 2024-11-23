fd90a459d365a2f34c949f7d2f307dbd35191e8b
Nile <nnaim36@gmail.com>
2024-11-23 05:42:03 -0500

    added nav bar to loginPage

--- app/src/main/java/edu/northeastern/group5_final/loginPage.java
+++ app/src/main/java/edu/northeastern/group5_final/loginPage.java
@@ -2,13 +2,24 @@ package edu.northeastern.group5_final;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class loginPage extends AppCompatActivity {
 
@@ -18,8 +29,27 @@ public class loginPage extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login_page);
 
+        //TODO: remove after finishing the nav bar
         Button btnSeach = findViewById(R.id.btn_lp_search);
         btnSeach.setOnClickListener(v -> {
+            startActivity(new Intent(this, SearchActivity.class));
+        });
+
+        Button btnSearch = findViewById(R.id.btn_search);
+        btnSearch.setOnClickListener(v -> {
+            startActivity(new Intent(this, SearchActivity.class));
+        });
+
+
+        // Set up the Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Other setup code
+
+        //TODO: remove after finishing the nav bar
+        Button btnSearch2 = findViewById(R.id.btn_lp_search);
+        btnSearch2.setOnClickListener(v -> {
             startActivity(new Intent(this, SearchActivity.class));
         });
 
@@ -28,5 +58,12 @@ public class loginPage extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.nav_menu, menu); // Use the correct menu file name
+        return true;
     }
 }
