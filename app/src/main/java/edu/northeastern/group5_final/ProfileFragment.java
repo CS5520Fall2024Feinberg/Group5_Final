e95336528f7a89fcf751d210053f9b537105f9dc
aryanjain28 <aryanjn807@gmail.com>
2024-11-28 11:42:24 -0500

    Added Dashboard and BottonNav bar

---
+++ app/src/main/java/edu/northeastern/group5_final/ProfileFragment.java
@@ -0,0 +1,30 @@
+package edu.northeastern.group5_final;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+
+public class ProfileFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button redirectButton = view.findViewById(R.id.login_page_btn);
+
+        redirectButton.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), loginPage.class);
+            startActivity(intent);
+        });
+    }
+}
