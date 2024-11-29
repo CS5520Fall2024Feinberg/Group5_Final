cec4eec8bce6337204117b3831061e01f71940b8
aryanjain28 <aryanjn807@gmail.com>
2024-11-29 16:14:58 -0500

    Added Log out logic

--- app/src/main/java/edu/northeastern/group5_final/ProfileFragment.java
+++ app/src/main/java/edu/northeastern/group5_final/ProfileFragment.java
@@ -9,7 +9,11 @@ import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class ProfileFragment extends Fragment {
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_profile, container, false);
@@ -21,10 +25,17 @@ public class ProfileFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         Button redirectButton = view.findViewById(R.id.login_page_btn);
-
         redirectButton.setOnClickListener(v -> {
             Intent intent = new Intent(getActivity(), loginPage.class);
             startActivity(intent);
         });
+
+        Button logoutredirectButton = view.findViewById(R.id.logout_btn);
+        logoutredirectButton.setOnClickListener(v -> {
+            firebaseAuth.signOut();
+            startActivity(new Intent(getActivity(), MainActivity.class));
+            getActivity().finish();
+        });
+
     }
 }
