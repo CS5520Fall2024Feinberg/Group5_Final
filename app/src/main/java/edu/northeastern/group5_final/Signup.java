6884c6f05b8f2e564ee3f3fe2f3788b5002c68c6
aryanjain28 <aryanjn807@gmail.com>
2024-12-08 01:10:26 -0500

    Added icon

--- app/src/main/java/edu/northeastern/group5_final/Signup.java
+++ app/src/main/java/edu/northeastern/group5_final/Signup.java
@@ -85,6 +85,11 @@ public class Signup extends AppCompatActivity {
         Button btnSignup = findViewById(R.id.btn_sign_up);
         btnSignup.setOnClickListener(view -> signUp());
 
+        Button btnSignin = findViewById(R.id.btn_sign_in);
+        btnSignin.setOnClickListener(view -> {
+            startActivity(new Intent(this, MainActivity.class));
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
